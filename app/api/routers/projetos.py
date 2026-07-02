588b5e26e27b25581b79c217009097e6bfbc51c2
Renef <renefricardo30@gmail.com>
2026-07-02 11:59:39 -0300

    Update Dockerfile and docker-compose.yml to reflect new app directory structure

--- app/api/routers/projetos.py
+++ app/api/routers/projetos.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter
+from starlette import status
 
 router = APIRouter(
     prefix="/projetos",
@@ -9,6 +10,6 @@ router = APIRouter(
 def listar_projetos():
     return []
 
-@router.get("/padrao")
+@router.get("/padrao", status_code=status.HTTP_200_OK)
 def lista_padrao():
     return ["projeto","projeto1","projeto2","projeto3"]
